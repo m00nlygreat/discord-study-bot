4d37ced5b33c0962799812a7f0d4bf7f7a1fefbe
sudole <resqpa79@gmail.com>
2023-06-26 22:25:17 +0900

    * 환경 변수 추가: GOOGLE JSON 인증 정보

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ TOKEN = '(str)<DISCORD_TOKEN>'
 # ex) CHANNEL_NAME = '(str)일반'
 CHANNEL_NAME = '(str)CHANNEL_NAME'
 VOICE_ROOM_NAME = '(str)VOICE_ROOM_NAME'
+# google auth
+GOOGLE_JSON_AUTH = '(str)'

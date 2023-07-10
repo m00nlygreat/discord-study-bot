67fbe43fc36d56e6ac87dd9968016ed29b8bc75f
sudole <resqpa79@gmail.com>
2023-07-10 21:17:09 +0900

    * 보이스룸 입/퇴장 시 구글 시트 기록, 메시지는 리액션만

--- services/dicord_manager.py
+++ services/dicord_manager.py
@@ -12,6 +12,15 @@ DS_CHANNEL_NAME = os.environ.get('CHANNEL_NAME')
 DS_VOICE_ROOM_NAME = os.environ.get('VOICE_ROOM_NAME')
 
 
+def check_channel_enter_type(before, after):
+    if before.channel is None or (before.channel is not None and after.channel is not None and (after.channel.name == VOICE_ROOM_NAME or after.channel.name == DS_VOICE_ROOM_NAME)):
+        return 'enter'
+    elif after.channel is None or (before.channel is not None and (before.channel.name == VOICE_ROOM_NAME or before.channel.name == DS_VOICE_ROOM_NAME)):
+        return 'leave'
+    else:
+        return ''
+
+
 class DiscordManager(discord.Client):
     attendance = {}
     concentration_time = {'_raw': []}
@@ -28,42 +37,15 @@ class DiscordManager(discord.Client):
 
     async def on_voice_state_update(self, user, before, after):
         # print(user, before, after)
-        user_info = '{0}#{1}'.format(user.name, user.discriminator)
+        person = f'{user.name}#{user.discriminator}' if user.discriminator != 0 else user.name
         if user == self.user:
             return
         if (before.channel is not None and (before.channel.name == VOICE_ROOM_NAME or before.channel.name == DS_VOICE_ROOM_NAME)) \
                 or (after.channel is not None and (after.channel.name == VOICE_ROOM_NAME or after.channel.name == DS_VOICE_ROOM_NAME)):
-            # make data
-            cc_data = {}
             now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
-            if before.channel is None or (before.channel is not None and after.channel is not None and (after.channel.name == VOICE_ROOM_NAME or after.channel.name == DS_VOICE_ROOM_NAME)):   # Enter
-                cc_data['start_time'] = now
-            elif after.channel is None or (before.channel is not None and (before.channel.name == VOICE_ROOM_NAME or before.channel.name == DS_VOICE_ROOM_NAME)):  # Exit
-                if user_info in self.concentration_time.keys():
-                    cc_data = self.concentration_time[user_info]
-                cc_data['end_time'] = now
-                # raw data add
-                cc_data['user'] = user_info
-                cc_data['total_hours'] = get_time_interval(cc_data['start_time'], cc_data['end_time'], "%Y-%m-%d %H:%M:%S", "hour")
-                self.concentration_time['_raw'].append(cc_data)
-
-            self.concentration_time[user_info] = cc_data
-
-    async def on_message(self, message):
-        # 봇 이벤트 인 경우 종료
-        if message.author == self.user:
-            return
-        # 설정된 채널인 경우만 아래 로직 수행
-        if message.channel.name == DS_CHANNEL_NAME or message.channel.name == CHANNEL_NAME:
-            user = message.author
-            person = f'{user.name}#{user.discriminator}' if user.discriminator != 0 else user.name
-            now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-            # 핑 테스트 용
-            if message.content == 'ping':
-                await message.channel.send('pong {0.author.mention}'.format(message))
-            # 출석 또는 출첵 단어가 포함된 경우
-            elif '출석' in message.content or '출첵' in message.content:
+            enter_type = check_channel_enter_type(before, after)
+            if enter_type == 'enter':
                 # make data
                 data = list()
                 data.append(now)                # (0) entry
@@ -78,9 +60,7 @@ class DiscordManager(discord.Client):
                 except gspread.exceptions.WorksheetNotFound:
                     self.g_service.add_worksheet('sessions', ['entry', 'leave', 'person', 'duration'])
                 self.g_service.add_row(data)
-
-                await message.add_reaction('👍')
-            elif '마무리' in message.content:
+            elif enter_type == 'leave':
                 # get sheet by name
                 try:
                     self.g_service.set_worksheet_by_name('sessions')
@@ -108,8 +88,23 @@ class DiscordManager(discord.Client):
                                 # update leave data
                                 self.g_service.worksheet.update(f'B{row_num}', now)
                                 self.g_service.worksheet.update(f'D{row_num}', get_time_interval(entry, now, "%Y-%m-%d %H:%M:%S"))
-                                await message.add_reaction('👍')
                                 return False
+
+    async def on_message(self, message):
+        # 봇 이벤트 인 경우 종료
+        if message.author == self.user:
+            return
+        # 설정된 채널인 경우만 아래 로직 수행
+        if message.channel.name == DS_CHANNEL_NAME or message.channel.name == CHANNEL_NAME:
+            user = message.author
+            person = f'{user.name}#{user.discriminator}' if user.discriminator != 0 else user.name
+            now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+            # 핑 테스트 용
+            if message.content == 'ping':
+                await message.channel.send('pong {0.author.mention}'.format(message))
+            # 출석/마무리는 리액션만 처리
+            elif '출석' in message.content or '출첵' in message.content or '마무리' in message.content:
+                await message.add_reaction('👍')
             elif '현황' in message.content or '조회' in message.content:
                 answer = get_attendance(self.attendance, self.concentration_time)
                 await message.channel.send(answer)
